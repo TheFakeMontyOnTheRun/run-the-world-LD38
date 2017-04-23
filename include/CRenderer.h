72eb94a331d14cac85c6b60d7deaf3edc0032e49
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-04-23 13:18:33 +0100

    Moving the structs to the header

--- include/CRenderer.h
+++ include/CRenderer.h
@@ -1,6 +1,24 @@
 #ifndef IRENDERER_H
 #define IRENDERER_H
 namespace odb {
+
+    struct Vec3 {
+        Vec3(float aX, float aY, float aZ) : x(aX), y(aY), z(aZ) {
+        }
+
+        float x = 0;
+        float y = 0;
+        float z = 0;
+    };
+
+    struct Vec2 {
+        Vec2(float aX, float aY) : x(aX), y(aY) {
+        }
+
+        float x = 0;
+        float y = 0;
+    };
+
     class CRenderer {
     private:
         CControlCallback mOnKeyPressedCallback;
